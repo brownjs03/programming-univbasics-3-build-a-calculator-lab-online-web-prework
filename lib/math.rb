884d972290551aa6a3f3f1d7fc0ed651095cad59
Jeffrey Brown <brownjs03@gmail.com>
2019-08-08 18:26:12 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,4 +1,5 @@
 def addition(num1, num2)
+<<<<<<< HEAD
   num1 + num2
 end
 
@@ -20,4 +21,33 @@ end
 
 def square_root(num)
 Math.sqrt(num)
+=======
+  addition = "#{num1 + num2}"
+  return addition
+end
+
+def subtraction(num1, num2)
+difference = "#{num1 - num2}"
+return difference
+end
+
+def division(num1, num2)
+quotient = "#{num1 / num2}"
+return quotient
+end
+
+def multiplication(num1, num2)
+product = "#{num1 * num2}"
+return product
+end
+
+def modulo(num1, num2)
+modulo = "#{num1 % num2}"
+return modulo
+end
+
+def square_root(num)
+square_root = "#{Math.sqrt(num)}"
+return square_root
+>>>>>>> 5b7727a32ac04fc53e58cb9b0c40d7331371e9ec
 end
